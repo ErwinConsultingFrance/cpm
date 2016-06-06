5d26515ab086e3ee9dfbdd82b1eb362cc248deda
Pouya Mohtacham <pouyajoon@gmail.com>
2016-06-06 11:51:42 +0200

    create layout

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 "# cpm" 
+
+
+add clone folder in path
+then go in evolve custom layout folder
+do 
+cpm --gl <layoutName>
--- index.js
+++ index.js
@@ -12,16 +12,55 @@
         console.log(o.join(''));
     }
 
+    function touch(filepath) {
+        console.info('touch file', filepath);
+        fs.closeSync(fs.openSync(filepath, 'w'));
+    }
+
+    function writeInFile(filepath, content) {
+        console.info('write in file', filepath);
+        fs.writeFile(filepath, content, function (err) {
+            if (err) return console.log(err);
+        });
+    }
+
+    function getJsonConfig(layoutName) {
+        return {
+            'Name': layoutName,
+            'DisplayName': layoutName,
+            'Description': layoutName,
+            'JavaScriptClassName': layoutName
+        }
+    }
+
+    function getPackageJsonConfig(layoutName) {
+        return {
+            'name': layoutName,
+            'description': layoutName,
+            'version': '0.0.0'
+        }
+    }
+
+    function getTemplateJsContent(layoutName) {
+        var fileContent = fs.readFileSync(__dirname + "/templates/template.js", 'utf8');
+        return fileContent.replace(/cwTemplateLayout/g, layoutName);
+    }
+
     function generateLayout(layoutName, callback) {
         fs.stat(layoutName, function (err, stats) {
             if (err !== null) {
                 fs.mkdir(layoutName, function (err) {
                     if (err) {
                         console.error(err);
+                    } else {
+                        writeInFile(layoutName + "/" + layoutName + ".js", getTemplateJsContent(layoutName));
+                        touch(layoutName + "/" + layoutName + ".less");
+                        writeInFile(layoutName + "/" + layoutName + ".layout.config.json", JSON.stringify(getJsonConfig(layoutName)));
+                        writeInFile(layoutName + "/package.json", JSON.stringify(getPackageJsonConfig(layoutName)));
+
                     }
                 });
             }
-            // console.log(err, stats);
         });
     }
 
