e4edfea270f16527e2bf5c426ea29a08b771c4a5
Pouya Mohtacham <pouyajoon@gmail.com>
2016-06-06 18:02:26 +0200

    add template

---
+++ templates/template.js
@@ -0,0 +1,29 @@
+/* Copyright (c) 2012-2016 Casewise Systems Ltd (UK) - All rights reserved */
+/*global cwAPI */
+
+(function (cwApi) {
+    "use strict";
+
+    var cwTemplateLayout = function (options, viewSchema) {
+        cwApi.extend(this, cwApi.cwLayouts.CwLayout, options, viewSchema);
+        this.drawOneMethod = this.drawOne.bind(this);
+        cwApi.registerLayoutForJSActions(this);
+    };
+
+    cwTemplateLayout.prototype.applyJavaScript = function () {        
+    };
+
+    cwTemplateLayout.prototype.drawOne = function (output, item, callback) {
+        /*jslint unparam:true*/
+    };
+
+    cwTemplateLayout.prototype.drawAssociations = function (output, associationTitleText, object) {
+        /*jslint unparam:true*/
+        this.domId = this.nodeID + '-' + this.options.LayoutID;
+        this.mainObject = object;
+        output.push("<div id='", this.domId, "'></div>");
+    };
+
+    cwApi.cwLayouts.cwTemplateLayout = cwTemplateLayout;
+
+}(cwAPI));
