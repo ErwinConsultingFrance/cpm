091366e694a22d87516f4baf00246223335b2cb7
nevakee716 <nevakee716@gmail.com>
2016-11-18 09:45:28 +0100

    update markdown to not use empty fields

--- libs/markDown/index.js
+++ libs/markDown/index.js
@@ -9,13 +9,13 @@ function UpdateReadmeMD(path, layoutsJson) {
     for (var layout in layoutsJson) {
         if (layoutsJson.hasOwnProperty(layout)) {
             output += "##" + layout + ": \n";
-            if (layoutsJson[layout].hasOwnProperty("description")) {
+            if (layoutsJson[layout].hasOwnProperty("description") && layoutsJson[layout].description != "") {
                 output += "- " + layoutsJson[layout].description + "\n";
             }
-            if (layoutsJson[layout].hasOwnProperty("wiki")) {
+            if (layoutsJson[layout].hasOwnProperty("wiki") && layoutsJson[layout].wiki != "" ) {
                 output += "- " + layoutsJson[layout].wiki + "\n";
             }
-            if (layoutsJson[layout].hasOwnProperty("evolve-versions")) {
+            if (layoutsJson[layout].hasOwnProperty("evolve-versions") && layoutsJson[layout]["evolve-versions"] != "") {
                 output += "- Compatibility : Evolve " + Object.keys(layoutsJson[layout]["evolve-versions"]) + "\n\n";
             }
         }
